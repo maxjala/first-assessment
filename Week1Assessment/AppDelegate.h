b6774f676c54b3234255745e82e0e547176672e5
Max Jala <maxjala1@macbook-pro.local>
2017-03-20 11:17:17 +0800

    Initial Commit

---
+++ Week1Assessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1Assessment
+//
+//  Created by Max Jala on 20/03/2017.
+//  Copyright © 2017 Max Jala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week1Assessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week1Assessment
+//
+//  Created by Max Jala on 20/03/2017.
+//  Copyright © 2017 Max Jala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
